71b0d1d4453edda3e5fef420115f12b70500942f
David Reguart <reguartdavid@gmail.com>
2023-04-04 02:10:35 +0200

    React app created & Tailwind installed

---
+++ web/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App bg-gradient-to-tr from-blue-700 via-blue-800 to-gray-900 h-screen text-white">
+      Hola
+    </div>
+  );
+}
+
+export default App;
